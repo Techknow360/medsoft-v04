0767e9baa75f2051dcd2f4747ba423aa2e89c87f
Parthiban <61075318+parthisri744@users.noreply.github.com>
2023-04-08 23:12:47 +0530

    Side Navigation And Top Header Component updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ViewComponent } from './view/view.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SidenavComponent } from './shared/base/sidenav/sidenav.component';
+import { FooterComponent } from './shared/base/footer/footer.component';
+import { TopheaderComponent } from './shared/base/topheader/topheader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ViewComponent,
-    SidenavComponent
+    SidenavComponent,
+    FooterComponent,
+    TopheaderComponent
   ],
   imports: [
     BrowserModule,
